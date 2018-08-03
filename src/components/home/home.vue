46e070a220f6c92824abbe1803ed60b6fc9fba3c
sundandan <1486620982@qq.com>
2018-08-03 11:58:12 +0800

    配置登录和home路由

---
+++ src/components/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+     <p>home组件</p>
+   </div>
+</template>
+  
+  <script>
+  export default {
+    name: 'App'
+  }
+  </script>
+  
+  <style>
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login/login'
+import Home from '@/components/home/home'
+// @是src路径的别名
 
 Vue.use(Router)
 
 export default new Router({
- 
+  routes: [
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'home',
+      path: '/home',
+      component: Home
+    }
+  ]
 })
